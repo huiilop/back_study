71d76344d131474dd2e8fb1fd6c395e9d52ca08d
huiilop <skyh@khu.ac.kr>
2022-07-16 00:54:53 +0900

    JPA 1 완강

--- KimYehui/jpashop/src/main/java/jpabook/jpashop/domain/service/OrderService.java
+++ KimYehui/jpashop/src/main/java/jpabook/jpashop/domain/service/OrderService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -47,12 +48,22 @@ public class OrderService {
     }
 
     /**
-     * 취소
+     * 주문취소
      */
-
+    @Transactional
+    public void cancelOrder(Long orderId){
+        // 주문 엔티티 조회
+        Order order = orderRepository.findOne(orderId);
+        // 주문 취소
+        order.cancel();
+    }
 
     /**
      * 검색
      */
-
+/*
+    public List<Order> findOrders(OrderSearch orderSearch){
+        return orderRepository.findAll(orderSearch);
+    }
+*/
 }
--- KimYehui/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ KimYehui/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -2,9 +2,16 @@ package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Tuple;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
+import javax.script.ScriptEngine;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -21,6 +28,89 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-  /*  public List<Order> findAll(OrderSearch orderSearch){
-    }*/
+    /**
+     * [방법1] 수동으로 문자 조립하는 복잡한 방법
+     * => 실무에서 잘 안씀
+     */
+    public List<Order> findAllByString(OrderSearch orderSearch){
+        /*
+        return em.createQuery("select o from order o join o.member m" +
+                " where o.status = :status " +
+                " and m.name like :name", Order.class)
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setParameter("name", orderSearch.getMemberName())
+                .setFirstResult(100) //페이징처리 100건씩
+                .setMaxResults(1000) //최대 1000건
+                .getResultList();
+         */ //위 방식대로 하면 입력값 없을 때 문제 생김. 동적 쿼리를 위해 아래와 같이 변경
+        String jpql = "select o from Order o join o.member m";
+        boolean isFirstCondition = true;
+
+        // 주문 상태 검색
+        if (orderSearch.getOrderStatus() != null){
+            if (isFirstCondition){
+                jpql += " where";
+                isFirstCondition = false;
+            } else{
+                jpql += " and";
+            }
+            jpql += " o.status = :status";
+        }
+
+        // 회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())){
+            if (isFirstCondition){
+                jpql += " where";
+                isFirstCondition = false;
+            } else{
+                jpql += " and";
+            }
+            jpql += " m.name like :name";
+        }
+
+        TypedQuery<Order> query = em.createQuery(jpql, Order.class)
+                .setMaxResults(1000);
+        if (orderSearch.getOrderStatus() != null){
+            query = query.setParameter("status", orderSearch.getOrderStatus());
+        }
+        if (org.springframework.util.StringUtils.hasText(orderSearch.getMemberName()){
+            query = query.setParameter("name", orderSearch.getMemberName());
+        }
+
+        return query.getResultList();
+    }
+
+    /**
+     * [방법2] JPA Criteria
+     * => 이것도 실무에서 잘 안쓰고 권장하지 않는 방법임
+     * => 치명적 단점 : 이거 보면 원큐에 도저히 알아먹지 못함. 유지보수가 불가한 수준이라함.
+     */
+    public List<Order> findAllByCriteria(OrderSearch orderSearch){
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+        Root<Order> o = cq.from(Order.class);
+        Join<Object, Object> m = o.join("member", JoinType.INNER);
+
+        List<Predicate> criteria = new ArrayList<>();
+
+        // 주문 상태 검색
+        if (orderSearch.getOrderStatus() != null){
+            Predicate status = cb.equal(o.get("status"), orderSearch.getOrderStatus());
+            criteria.add(status);
+        }
+        // 회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())){
+            Predicate name = cb.like(m.<String>get("name"), "%"+orderSearch.getMemberName()+"%");
+            criteria.add(name);
+        }
+
+        cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
+        TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000);
+        return query.getResultList();
+    }
+    /**
+     * [방법3] Query DSL
+     * => 추천!
+     */
+
 }
